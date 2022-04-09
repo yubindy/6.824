18b0a8eb28a83f4c7f1098a430df58bf7578e8e0
yuxubinchen <2302370185@qq.com>
2022-04-09 23:01:46 +0800

    update lab1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 )
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
-type Taskinter interface {
+type Queue interface {
+	push()
+	pop()
+	isempty()
+}
+type taskqueue struct { //用于重新分配的任务
+	Knum [100]int
+	mut  sync.Mutex
+	frn  int
+	end  int
 }
 
 const (
@@ -31,7 +41,7 @@ type Coordinator struct {
 	times   map[int]int //节点对应时间映射
 	files   []string
 	mut     sync.RWMutex
-	cond    sync.Cond
+	queue   taskqueue
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -45,48 +55,86 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
+func (q *taskqueue) push(K int) bool {
+	q.mut.Lock()
+	q.Knum[q.end] = K
+	q.end = (q.end + 1) % 100
+	q.mut.Unlock()
+	return true
+}
+func (q *taskqueue) pop() int {
+	q.mut.Lock()
+	tnum := q.Knum[q.frn]
+	q.frn = (q.frn + 1) % 100
+	q.mut.Unlock()
+	return tnum
+}
+func (q *taskqueue) isempty() bool {
+	if q.frn == q.end {
+		return true
+	} else {
+		return false
+	}
+
+}
 func (c *Coordinator) Getinfo(args *Args, reply *Reply) error { //分配任务
-	c.stat[args.tasknum] = args.localstate
-	ok := c.stat[args.tasknum]
+	var ok int
+	if args.tasknum != -1 {
+		c.stat[args.tasknum] = args.localstate
+		ok = c.stat[args.tasknum]
+	} else {
+		ok = waiting
+	}
 	if ok == doingreduce {
-		c.Map(args, reply)
+		c.Reduce(args, reply)
 	} else if ok == waiting {
 		c.mut.RLock()
 		nmap := c.nmap
 		c.mut.RUnlock()
-		if nmap < len(c.files) {
+		if nmap < c.smap {
 			c.Map(args, reply)
 		} else {
 			reply.t = waiting
 		}
-
 	} else { //Map
-		c.Reduce(args, reply)
+		c.Map(args, reply)
 	}
 	return nil
 }
 func (c *Coordinator) Map(args *Args, reply *Reply) {
-	if c.nmap >= len(c.files) {
+	if c.nmap >= c.smap {
 		reply.t = WorkWait
-		c.stat[args.tasknum] = waiting
 	}
 	reply.t = WorkMap
-	c.times[args.tasknum] = 10 //将该状态下的任务定为10s
-	c.stat[args.tasknum] = doingmap
-	c.mut.Lock()
-	c.nmap++
-	c.mut.Unlock()
-	reply.num = c.nmap
+	if !c.queue.isempty() {
+		reply.num = c.queue.pop()
+		c.nmap++
+	} else {
+		c.mut.Lock()
+		c.nmap++
+		c.mut.Unlock()
+		reply.num = c.nmap
+	}
+	c.times[reply.num] = 10 //将该状态下的任务定为10s
+	c.stat[reply.num] = doingmap
 	reply.filepath[0] = c.files[reply.num]
 }
 func (c *Coordinator) Reduce(args *Args, reply *Reply) {
+	if c.nreduce >= c.sreduce {
+		reply.t = WorkWait
+	}
 	reply.t = WorkReduce
-	c.times[args.tasknum] = 10 //将该状态下的任务定为10s
-	c.stat[args.tasknum] = doingreduce
-	c.mut.Lock()
-	c.nreduce++
-	c.mut.Unlock()
-	reply.num = c.nreduce
+	if !c.queue.isempty() {
+		reply.num = c.queue.pop()
+		c.nreduce++
+	} else {
+		c.mut.Lock()
+		c.nreduce++
+		c.mut.Unlock()
+		reply.num = c.nreduce
+	}
+	c.times[reply.num] = 10 //将该状态下的任务定为10s
+	c.stat[reply.num] = doingreduce
 	for i := 0; i < c.nmap; i++ {
 		reply.filepath[i] = fmt.Sprintf("mr-%v%v", i, c.nreduce)
 	}
@@ -95,6 +143,25 @@ func (c *Coordinator) Reduce(args *Args, reply *Reply) {
 //
 // start a thread that listens for RPCs from worker.go
 //
+
+func (c *Coordinator) checktime() {
+	for {
+		for a, b := range c.times {
+			if c.stat[a] != waiting {
+				c.times[a] = b - 1
+				if c.times[a] == 0 {
+					c.queue.push(a)
+					if c.stat[a] == doingreduce {
+						c.nreduce--
+					} else if c.stat[a] == doingmap {
+						c.nmap--
+					}
+				}
+			}
+		}
+	}
+	time.Sleep(time.Second)
+}
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -115,7 +182,7 @@ func (c *Coordinator) server() {
 func (c *Coordinator) Done() bool {
 	ret := false
 	c.server()
-
+	go c.checktime()
 	// Your code here.
 	return ret
 }
@@ -127,10 +194,8 @@ func (c *Coordinator) Done() bool {
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-	c.nmap = 0
 	c.smap = len(files)
 	c.sreduce = nReduce
-	c.nreduce = 0
 	c.files = files
 	c.server()
 	return &c
